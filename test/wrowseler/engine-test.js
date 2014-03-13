8511bee13d0e991639e589010132e92f67fc6bdc
Ivan Fraixedes <ifcdev@gmail.com>
2014-03-13 23:55:23 +0000

    Added tests for checking Engine with a base sequence

--- test/wrowseler/engine-test.js
+++ test/wrowseler/engine-test.js
@@ -23,12 +23,11 @@ describe('Browserler engine', function () {
   });
 
   describe('is instantiated without any base sequence', function () {
-    var browser = {};
     var engine;
 
     before(function () {
       engine = new Engine({
-        browser: browser
+        browser: {}
       });
     });
 
@@ -97,6 +96,153 @@ describe('Browserler engine', function () {
       });
 
       for (let si = 0; si < numSteps; si++) {
+        it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
+          function waitUntilStepEnds() {
+            if (idx < stepsArguments.length) {
+              let stepArgs = stepsArguments[idx];
+              stepArgs.should.have.length(idx + 2);
+              stepArgs[0].should.deep.equals(engine.browser);
+
+              for (let ai = 2; ai < stepArgs.length; ai++) {
+                stepArgs[ai].should.equals(stepArgs[ai - 1] + 1);
+              }
+
+              done();
+            } else {
+              setImmediate(waitUntilStepEnds);
+            }
+          }
+
+          waitUntilStepEnds(idx);
+        }.bind(null, si));
+      }
+
+      it('when task finish, we get back a task done object with its id', function (done) {
+        function waitUntilEnd() {
+          if (0 < tasksDone.length) {
+            let taskDone = tasksDone[0];
+            tasksDone.should.length(1);
+            taskDone.should.have.a.property('id', taskId);
+            taskDone.should.have.a.property('result', undefined);
+            done();
+          } else {
+            setImmediate(waitUntilEnd);
+          }
+        }
+
+        waitUntilEnd();
+      });
+    });
+  });
+
+  describe('is instantiated with a base sequence', function () {
+    function fakeStep(stepsArguments) {
+      return function () {
+        var generator = arguments[0];
+        var args = [].slice.call(arguments, 1);
+        var argsToPassNext = args.slice(1);
+
+        stepsArguments.push(args);
+        argsToPassNext.push(argsToPassNext[argsToPassNext.length - 1] + 1);
+        console.log(stepsArguments.length);
+
+        setImmediate(function () {
+          generator.next.call(generator, argsToPassNext);
+        });
+      };
+    }
+
+    var engine;
+    var stepsArguments = [];
+    var numBaseSteps = 3;
+
+    before(function () {
+      var baseSequence = [];
+
+      for (let si = 0; si < numBaseSteps; si++) {
+        baseSequence.push(fakeStep(stepsArguments));
+      }
+
+      engine = new Engine({
+        browser: {},
+        sequence: baseSequence
+      });
+    });
+
+    describe('and it runs without any senquence', function () {
+      function taskDoneListener(task) {
+        tasksDone.push(task);
+      }
+
+      var tasksDone = [];
+      var taskId;
+
+      before(function () {
+        engine.on('task-done', taskDoneListener);
+        taskId = engine.run(null, 0);
+      });
+
+      it('return a task object', function () {
+        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+      });
+
+      for (let si = 0; si < numBaseSteps; si++) {
+        it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
+          function waitUntilStepEnds() {
+            if (idx < stepsArguments.length) {
+              let stepArgs = stepsArguments[idx];
+              stepArgs.should.have.length(idx + 2);
+              stepArgs[0].should.deep.equals(engine.browser);
+
+              for (let ai = 2; ai < stepArgs.length; ai++) {
+                stepArgs[ai].should.equals(stepArgs[ai - 1] + 1);
+              }
+
+              done();
+            } else {
+              setImmediate(waitUntilStepEnds);
+            }
+          }
+
+          waitUntilStepEnds(idx);
+        }.bind(null, si));
+      }
+
+      it('when task finish, we get back a task done object with its id', function () {
+        var taskDone = tasksDone[0];
+        tasksDone.should.length(1);
+        taskDone.should.have.a.property('id', taskId);
+        taskDone.should.have.a.property('result', undefined);
+      });
+    });
+
+    describe('and it runs with a step sequences', function () {
+      function taskDoneListener(task) {
+        tasksDone.push(task);
+      }
+
+      var tasksDone = [];
+      var numSteps = 3;
+      var taskId;
+
+      before(function () {
+        var steps = [];
+
+        stepsArguments.splice(0, stepsArguments.length);
+        engine.on('task-done', taskDoneListener);
+
+        for (let si = 0; si < numSteps; si++) {
+          steps.push(fakeStep(stepsArguments));
+        }
+
+        taskId = engine.run(steps, 0);
+      });
+
+      it('return a task object', function () {
+        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+      });
+
+      for (let si = 0; si < numSteps + numBaseSteps; si++) {
         it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
           function waitUntilStepEnds() {
             if (idx < stepsArguments.length) {
