0e712b440aa17dce6600d6003c402ba335796696
Ivan Fraixedes <ifcdev@gmail.com>
2014-03-15 15:15:55 +0000

    Swapped from should to expect assertion style and fixed bug
    I swapped from `should` to `expect` assertion style to have a nicer way
    to check `undefined` values; furthermore I found an issue in engine with
    task done results after added some assertions to test it, so I fixed it.

--- test/wrowseler/engine-test.js
+++ test/wrowseler/engine-test.js
@@ -3,8 +3,8 @@
 var chai = require('chai');
 var Engine = require('../../lib/wrowseler/engine');
 
-describe('Browserler engine', function () {
-  var should = chai.should();
+describe.only('Browserler engine', function () {
+  var expect = chai.expect;
 
   describe('is instanitated without any parameters', function () {
     it('throws an error', function () {
@@ -13,8 +13,8 @@ describe('Browserler engine', function () {
       try {
         engine = new Engine();
       } catch (e) {
-        e.should.instanceOf(Error);
-        e.message.should.match(/Cannot read property/);
+        expect(e).instanceOf(Error);
+        expect(e).match(/Cannot read property/);
         return;
       }
 
@@ -45,14 +45,15 @@ describe('Browserler engine', function () {
       });
 
       it('return a task object', function () {
-        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+        expect(taskId).match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       });
 
-      it('when task finish, we get back a task done object with its id', function () {
+      it('when task finishes, it returns an object with task\'s id and results is undefined', function () {
         var taskDone = tasksDone[0];
-        tasksDone.should.length(1);
-        taskDone.should.have.a.property('id', taskId);
-        taskDone.should.have.a.property('result', undefined);
+        expect(tasksDone).to.have.length(1);
+        expect(taskDone).to.have.ownProperty('id', taskId);
+        expect(taskDone).to.have.ownProperty('results');
+        expect(taskDone.results).to.equal(undefined);
       });
     });
 
@@ -92,45 +93,17 @@ describe('Browserler engine', function () {
       });
 
       it('return a task object', function () {
-        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+        expect(taskId).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       });
 
       for (let si = 0; si < numSteps; si++) {
         it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
-          function waitUntilStepEnds() {
-            if (idx < stepsArguments.length) {
-              let stepArgs = stepsArguments[idx];
-              stepArgs.should.have.length(idx + 2);
-              stepArgs[0].should.deep.equals(engine.browser);
-
-              for (let ai = 2; ai < stepArgs.length; ai++) {
-                stepArgs[ai].should.equals(stepArgs[ai - 1] + 1);
-              }
-
-              done();
-            } else {
-              setImmediate(waitUntilStepEnds);
-            }
-          }
-
-          waitUntilStepEnds(idx);
+          waitUntilStepEnds(expect, engine, stepsArguments, idx, done);
         }.bind(null, si));
       }
 
-      it('when task finish, we get back a task done object with its id', function (done) {
-        function waitUntilEnd() {
-          if (0 < tasksDone.length) {
-            let taskDone = tasksDone[0];
-            tasksDone.should.length(1);
-            taskDone.should.have.a.property('id', taskId);
-            taskDone.should.have.a.property('result', undefined);
-            done();
-          } else {
-            setImmediate(waitUntilEnd);
-          }
-        }
-
-        waitUntilEnd();
+      it('when task finishes, it returns an object with task\'s id and results', function (done) {
+        waitTaskUntilDone(expect, taskId, tasksDone, stepsArguments, done);
       });
     });
   });
@@ -181,36 +154,17 @@ describe('Browserler engine', function () {
       });
 
       it('return a task object', function () {
-        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+        expect(taskId).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       });
 
       for (let si = 0; si < numBaseSteps; si++) {
         it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
-          function waitUntilStepEnds() {
-            if (idx < stepsArguments.length) {
-              let stepArgs = stepsArguments[idx];
-              stepArgs.should.have.length(idx + 2);
-              stepArgs[0].should.deep.equals(engine.browser);
-
-              for (let ai = 2; ai < stepArgs.length; ai++) {
-                stepArgs[ai].should.equals(stepArgs[ai - 1] + 1);
-              }
-
-              done();
-            } else {
-              setImmediate(waitUntilStepEnds);
-            }
-          }
-
-          waitUntilStepEnds(idx);
+          waitUntilStepEnds(expect, engine, stepsArguments, idx, done);
         }.bind(null, si));
       }
 
-      it('when task finish, we get back a task done object with its id', function () {
-        var taskDone = tasksDone[0];
-        tasksDone.should.length(1);
-        taskDone.should.have.a.property('id', taskId);
-        taskDone.should.have.a.property('result', undefined);
+      it('when task finishes, it returns an object with task\'s id and results', function (done) {
+        waitTaskUntilDone(expect, taskId, tasksDone, stepsArguments, done);
       });
     });
 
@@ -237,45 +191,17 @@ describe('Browserler engine', function () {
       });
 
       it('return a task object', function () {
-        taskId.should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+        expect(taskId).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       });
 
       for (let si = 0; si < numSteps + numBaseSteps; si++) {
         it('step ' + si + ' has been called with the agument number of the previous step and added new argument which it is the previous number plus 1', function (idx, done) {
-          function waitUntilStepEnds() {
-            if (idx < stepsArguments.length) {
-              let stepArgs = stepsArguments[idx];
-              stepArgs.should.have.length(idx + 2);
-              stepArgs[0].should.deep.equals(engine.browser);
-
-              for (let ai = 2; ai < stepArgs.length; ai++) {
-                stepArgs[ai].should.equals(stepArgs[ai - 1] + 1);
-              }
-
-              done();
-            } else {
-              setImmediate(waitUntilStepEnds);
-            }
-          }
-
-          waitUntilStepEnds(idx);
+          waitUntilStepEnds(expect, engine, stepsArguments, idx, done);
         }.bind(null, si));
       }
 
-      it('when task finish, we get back a task done object with its id', function (done) {
-        function waitUntilEnd() {
-          if (0 < tasksDone.length) {
-            let taskDone = tasksDone[0];
-            tasksDone.should.length(1);
-            taskDone.should.have.a.property('id', taskId);
-            taskDone.should.have.a.property('result', undefined);
-            done();
-          } else {
-            setImmediate(waitUntilEnd);
-          }
-        }
-
-        waitUntilEnd();
+      it('when task finishes, it returns an object with task\'s id and results', function (done) {
+        waitTaskUntilDone(expect, taskId, tasksDone, stepsArguments, done);
       });
     });
   });
@@ -291,11 +217,44 @@ describe('Browserler engine', function () {
     });
 
     it('so when run the first task, a task id is returned', function () {
-      engine.run().should.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+      expect(engine.run()).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
     });
 
     it('however if another task is run before a previous one is finished then throw an Error', function () {
-      engine.run.bind(engine).should.throws(Error, 'The engine is running one task. So far only one task can be executed a time');
+      expect(engine.run.bind(engine)).to.throws(Error, 'The engine is running one task. So far only one task can be executed a time');
     });
   });
 });
+
+function waitTaskUntilDone(expect, taskId, tasksDone, stepsArguments, done) {
+  if (0 < tasksDone.length) {
+    let taskDone = tasksDone[0];
+    let expectedResults = stepsArguments[stepsArguments.length - 1].slice(1);
+
+    expectedResults.push(expectedResults[expectedResults.length - 1] + 1);
+    expect(tasksDone).to.have.length(1);
+    expect(taskDone).to.have.ownProperty('id', taskId);
+    expect(taskDone).to.have.ownProperty('results');
+    expect(taskDone.results).to.eql(expectedResults);
+    done();
+  } else {
+    setImmediate(waitTaskUntilDone.apply.bind(null, null, arguments));
+  }
+}
+function waitUntilStepEnds(expect, engine, stepsArguments, idx, done) {
+  if (idx < stepsArguments.length) {
+    let stepArgs = stepsArguments[idx];
+
+    expect(stepArgs).to.have.length(idx + 2);
+    expect(stepArgs[0]).deep.equals(engine.browser);
+
+    for (let ai = 2; ai < stepArgs.length; ai++) {
+      expect(stepArgs[ai]).to.equals(stepArgs[ai - 1] + 1);
+    }
+
+    done();
+  } else {
+    setImmediate(waitUntilStepEnds.apply.bind(null, null, arguments));
+  }
+}
+
