48b15bf7e82a99b7c9e1c011f0598bdf71ed7771
Ivan Fraixedes <ifcdev@gmail.com>
2014-04-20 20:13:33 +0100

    New helper function `rods/browser/index#injectAsyncScriptBlock` and its test
    The new function shold allow to inject block scripts in webdriver and
    call one of the functions defined in the block with the provided
    arguments using  `webdriver#executeAsync`.
    
    I also fixed some specifications texts in `test/rods/browser/helpers-test.js`

--- test/rods/browser/helpers-test.js
+++ test/rods/browser/helpers-test.js
@@ -308,5 +308,144 @@ describe('Browser rods base functions', function () {
       });
     });
   });
+
+  /************************* hey you*/
+  describe('`injectAsyncScriptBlock`', function () {
+    describe('when called without options and without arguments', function () {
+      var scriptBlock, wbChrome, stub;
+
+      before(function () {
+        wbChrome = new seleniumWb.Builder()
+        .withCapabilities(seleniumWb.Capabilities.chrome())
+        .build();
+
+        stub = sinon.stub(wbChrome, 'executeAsyncScript');
+        scriptBlock = rods.objectPropertiesToScript({
+          execMe: function () {
+            //done()
+          }
+        });
+        rods.injectAsyncScriptBlock(wbChrome, scriptBlock, 'execMe');
+      });
+
+      it('`webdriver executeAsyncScript` method is executed with a script that wraps the function and defines a variable called `done`', function () {
+        var expectedScript =
+          ' var scriptArgs = \\[\\]\\.slice\\.call\\(arguments, 0\\);' +
+          ' var done = scriptArgs\\.pop\\(\\);' +
+          ' execMe\\.apply\\(null, scriptArgs\\);';
+
+        expect(stub.calledOnce).to.be.true;
+        expect(stub.args[0][0]).to.match(new RegExp(expectedScript));
+        expect(stub.args[0][0]).to.match(/var execMe=function \(\)/);
+      });
+
+      it ('`webdriver executeAsyncScript` is called without any argument more', function () {
+        expect(stub.args[0]).to.length(1);
+      });
+    });
+
+    describe('when called without options and with arguments', function () {
+      var scriptBlock, wbChrome, stub;
+
+      before(function () {
+        wbChrome = new seleniumWb.Builder()
+        .withCapabilities(seleniumWb.Capabilities.chrome())
+        .build();
+
+        stub = sinon.stub(wbChrome, 'executeAsyncScript');
+        scriptBlock = rods.objectPropertiesToScript({
+          execMe: function () {
+            //done()
+          }
+        });
+        rods.injectAsyncScriptBlock(wbChrome, scriptBlock, 'execMe', 'argument 1', 'argument 2');
+      });
+
+      it('`webdriver executeAsyncScript` method is executed with a script that wraps the function and defines a variable called `done`', function () {
+        var expectedScript =
+          ' var scriptArgs = \\[\\]\\.slice\\.call\\(arguments, 0\\);' +
+          ' var done = scriptArgs\\.pop\\(\\);' +
+          ' execMe\\.apply\\(null, scriptArgs\\);';
+
+        expect(stub.calledOnce).to.be.true;
+        expect(stub.args[0][0]).to.match(new RegExp(expectedScript));
+        expect(stub.args[0][0]).to.match(/var execMe=function \(\)/);
+      });
+
+      it ('`webdriver executeAsyncScript` is called with the provided arguments', function () {
+        expect(stub.args[0]).to.length(3);
+        expect(stub.args[0][1]).to.equal('argument 1');
+        expect(stub.args[0][2]).to.equal('argument 2');
+      });
+    });
+
+    describe('when called with the option `callbackName` to "myDone" and without arguments', function () {
+      var scriptBlock, wbChrome, stub;
+
+      before(function () {
+        wbChrome = new seleniumWb.Builder()
+        .withCapabilities(seleniumWb.Capabilities.chrome())
+        .build();
+
+        stub = sinon.stub(wbChrome, 'executeAsyncScript');
+        scriptBlock = rods.objectPropertiesToScript({
+          execMe: function () {
+            //myDone()
+          }
+        });
+        rods.injectAsyncScriptBlock(wbChrome, { callbackName: 'myDone' }, scriptBlock, 'execMe');
+      });
+
+      it('`webdriver executeAsyncScript` method is executed with a script that wraps the function and defines a variable called `myDone`', function () {
+        var expectedScript =
+          ' var scriptArgs = \\[\\]\\.slice\\.call\\(arguments, 0\\);' +
+          ' var myDone = scriptArgs\\.pop\\(\\);' +
+          ' execMe\\.apply\\(null, scriptArgs\\);';
+
+        expect(stub.calledOnce).to.be.true;
+        expect(stub.args[0][0]).to.match(new RegExp(expectedScript));
+        expect(stub.args[0][0]).to.match(/var execMe=function \(\)/);
+      });
+
+      it ('`webdriver executeAsyncScript` is called without any argument more', function () {
+        expect(stub.args[0]).to.length(1);
+      });
+    });
+
+    describe('when called with the option `callbackName` to "myDone" and with arguments', function () {
+      var scriptBlock, wbChrome, stub;
+
+      before(function () {
+        wbChrome = new seleniumWb.Builder()
+        .withCapabilities(seleniumWb.Capabilities.chrome())
+        .build();
+
+        stub = sinon.stub(wbChrome, 'executeAsyncScript');
+        scriptBlock = rods.objectPropertiesToScript({
+          execMe: function () {
+            //myDone()
+          }
+        });
+        rods.injectAsyncScriptBlock(wbChrome, { callbackName: 'myDone' }, scriptBlock, 'execMe', 'argument 1', 'argument 2');
+      });
+
+      it('`webdriver executeAsyncScript` method is executed with a script that wraps the function and defines a variable called `myDone`', function () {
+        var expectedScript =
+          ' var scriptArgs = \\[\\]\\.slice\\.call\\(arguments, 0\\);' +
+          ' var myDone = scriptArgs\\.pop\\(\\);' +
+          ' execMe\\.apply\\(null, scriptArgs\\);';
+
+        expect(stub.calledOnce).to.be.true;
+        expect(stub.args[0][0]).to.match(new RegExp(expectedScript));
+        expect(stub.args[0][0]).to.match(/var execMe=function \(\)/);
+      });
+
+      it ('`webdriver executeAsyncScript` is called with the provided arguments', function () {
+        expect(stub.args[0]).to.length(3);
+        expect(stub.args[0][1]).to.equal('argument 1');
+        expect(stub.args[0][2]).to.equal('argument 2');
+      });
+    });
+  });
 });
 
