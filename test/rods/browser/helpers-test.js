6302257cacbce252acd468ea990cfcfc13d4a285
Ivan Fraixedes <ifcdev@gmail.com>
2014-04-20 18:42:39 +0100

    New helper method in `rods/browser` to transform object props to script
    Added new helper method in `rods/browser` to transform object properties
    to string and to be able to inject them as script

--- test/rods/browser/helpers-test.js
+++ test/rods/browser/helpers-test.js
@@ -195,5 +195,45 @@ describe('Browser rods base functions', function () {
       });
     });
   });
+
+  describe('`objectPropertiesToScript` transform to script all the enumerable properties of an object', function () {
+    var objToScript, generatedScript; 
+
+    before(function () {
+      var prop7Value = {
+        name: 'test',
+        toString: function () { return JSON.stringify(this); }
+      };
+
+      objToScript = {
+        prop1: function () { return 'prop1'; },
+        prop2: function prop2() { return 'prop2'; },
+        prop3: 'a string',
+        prop4: 10,
+        prop5: /regularExp/,
+        prop6: {},
+        prop7: prop7Value
+      };
+
+      generatedScript = rods.objectPropertiesToScript(objToScript);
+    });
+
+    it('generating a script which contains all the enumerable function properties as defined variables', function () {
+      expect(generatedScript).to.match(/var prop1=/);
+      expect(generatedScript).to.match(/var prop2=/);
+      expect(generatedScript).to.match(/function prop2\(\)/);
+    });
+
+    it('generating a script which contains the string variables defined as strings', function () {
+      expect(generatedScript).to.match(/var prop3='a string'/);
+    });
+
+    it('generating a script which contains all the other types to their string representation ', function () {
+      expect(generatedScript).to.match(/var prop4=10/);
+      expect(generatedScript).to.match(/var prop5=\/regularExp\//);
+                                       expect(generatedScript).to.match(/var prop6=\[object Object\]/);
+      expect(generatedScript).to.match(new RegExp('var prop7=' + objToScript.prop7.toString()));
+    });
+  });
 });
 
