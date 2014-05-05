d8fd7a5ce041756f6ec0572422d73dd5975f1751
Ivan Fraixedes <ifcdev@gmail.com>
2014-05-05 15:39:47 +0100

    Added events on `engine` for step starts/ends
    I added to `engine` two events emission when, one just before a sequence's
    step executes and another just after a sequence's step ends

--- test/wrowseler/engine-test.js
+++ test/wrowseler/engine-test.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var chai = require('chai');
+var _ = require('lodash');
 var Engine = require('../../lib/wrowseler/engine');
+
+var _clone = _.clone;
+var _last = _.last;
 
 describe('Browserler engine', function () {
   var expect = chai.expect;
@@ -35,11 +39,23 @@ describe('Browserler engine', function () {
         tasksDone.push(task);
       }
 
+      function stepSeqStarts(args) {
+        stepSeqStartsCounter++;
+      }
+
+      function stepSeqEnds(result) {
+        stepSeqEndsCounter++;
+      }
+
       var tasksDone = [];
       var taskId;
+      var stepSeqStartsCounter = 0;
+      var stepSeqEndsCounter = 0;
 
       before(function () {
         engine.on(Engine.EVENT_TASK_DONE, taskDoneListener);
+        engine.on(Engine.EVENT_STEP_SEQUENCE_STARTS, stepSeqStarts);
+        engine.on(Engine.EVENT_STEP_SEQUENCE_ENDS, stepSeqEnds);
         taskId = engine.speedUp();
       });
 
@@ -49,6 +65,8 @@ describe('Browserler engine', function () {
 
       it('when task finishes, it returns an object with task\'s id, status of "ok" and results is undefined', function () {
         var taskDone = tasksDone[0];
+        expect(stepSeqStartsCounter).to.equal(0);
+        expect(stepSeqEndsCounter).to.equal(0);
         expect(tasksDone).to.have.length(1);
         expect(taskDone).to.have.property('id', taskId);
         expect(taskDone).to.have.property('status', Engine.TASK_STATUS_OK);
@@ -60,6 +78,16 @@ describe('Browserler engine', function () {
     describe('and it runs with a step sequences', function () {
       function taskDoneListener(task) {
         tasksDone.push(task);
+      }
+
+      function stepSeqStarts(args) {
+        stepSeqStartsArgs = args;
+        stepSeqStartsCounter++;
+      }
+
+      function stepSeqEnds(result) {
+        stepSeqEndsResult = result;
+        stepSeqEndsCounter++;
       }
 
       function fakeStep() {
@@ -78,12 +106,18 @@ describe('Browserler engine', function () {
       var stepsArguments = [];
       var tasksDone = [];
       var numSteps = 3;
+      var stepSeqStartsCounter = 0;
+      var stepSeqStartsArgs;
+      var stepSeqEndsCounter = 0;
+      var stepSeqEndsResult;
       var taskId;
 
       before(function () {
         var steps = [];
 
         engine.on(Engine.EVENT_TASK_DONE, taskDoneListener);
+        engine.on(Engine.EVENT_STEP_SEQUENCE_STARTS, stepSeqStarts);
+        engine.on(Engine.EVENT_STEP_SEQUENCE_ENDS, stepSeqEnds);
 
         for (let si = 0; si < numSteps; si++) {
           steps.push(fakeStep);
@@ -104,6 +138,27 @@ describe('Browserler engine', function () {
 
       it('when task finishes, it returns an object with task\'s id and results', function (done) {
         waitTaskUntilDone(expect, taskId, tasksDone, stepsArguments, done);
+      });
+
+      it('when task finishes the event step sequence start has been called as many times as number of steps and last time received the same argumes as the last step', function (done) {
+        function waitUntilEventStepsReceived() {
+          if ((stepSeqStartsCounter < numSteps) || (stepSeqEndsCounter < numSteps)) {
+            setTimeout(waitUntilEventStepsReceived, 100);
+          } else {
+            let lastStepArgs = _clone(_last(stepsArguments));
+            lastStepArgs.shift();
+
+            expect(stepSeqStartsCounter).to.equal(numSteps);
+            expect(stepSeqEndsCounter).to.equal(numSteps);
+            expect(stepSeqStartsArgs).to.have.property('arguments');
+            expect(stepSeqStartsArgs.arguments).to.eql(lastStepArgs);
+            expect(stepSeqEndsResult).to.have.property('result');
+            expect(stepSeqEndsResult.result).to.eql(tasksDone[0].results);
+            done();
+          }
+        }
+
+        waitUntilEventStepsReceived();
       });
     });
   });
@@ -235,11 +290,11 @@ describe('Browserler engine', function () {
 
     before(function () {
       var steps = [];
-      
+
       for (let si = 0; si < numSteps; si++) {
         steps.push(fakeStep);
       }
-      
+
       engine = new Engine({
         browser: {},
         sequence: steps
@@ -308,12 +363,12 @@ describe('Browserler engine', function () {
       });
     });
   });
-  
+
   describe('instance which runs two steps sequences but it is stopped before finishes', function () {
     function taskDoneListener(task) {
       tasksDone.push(task);
     }
-    
+
     function delayedSequenceStep(generator, browser) {
       setTimeout(function () { generator.next('executed'); }, 300);
     }
@@ -351,7 +406,7 @@ describe('Browserler engine', function () {
         done();
       }, 500);
     });
-    
+
     it('task 2 doesn\'t finish in the stoped cycle', function (done) {
       setTimeout(function () {
         var taskDone = tasksDone[0];
@@ -364,7 +419,7 @@ describe('Browserler engine', function () {
       before(function () {
         engine.switchOn();
       });
-      
+
       it('task 2 finishes with status of "ok" and the expected results value', function (done) {
         setTimeout(function () {
           var taskDone = tasksDone[1];
